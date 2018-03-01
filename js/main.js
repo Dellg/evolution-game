a1678fbe21c54d4869a62f60bdbc26eb6e191323
Wendell Gomes <wendell_rpg@hotmail.com>
2018-02-28 21:46:08 -0300

    Adicionado modo debug novamente.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var criatura = null;
 var levelnum = 1;
 var level = null;
 var imagens = [];
+var debug = false;
 
 //______________________________________________________________________________
 // carregando imagens no projeto
@@ -138,7 +139,38 @@ function draw(){
   // interface do jogo
   } else if (menu == 1){
     level.rodar();
+    fill(255);
+    if (this.debug){
+      text("Alimentos:", 10, 60);
+      text("- com cor em tom de verde/verde-limão são plantas;", 10, 80);
+      text("- com cor em tom de vermelho/laranja são insetos;", 10, 100);
+      text("- com cor em tom de azul/roxo são comidas tóxicas;", 10, 120);
+      text("- com cor branca são carnes de animais mortos.", 10, 140);
+      text("Criaturas:", 10, 170);
+      text("- com formato de losango são herbívoros;", 10, 190);
+      text("- com formato de triângulo são onívoros;", 10, 210);
+      text("- com formato de seta são carnívoros.", 10, 230);
+      text("Aura das criaturas:", 10, 260);
+      text("- verde é a área de percepção para plantas;", 10, 280);
+      text("- vermelho é a área de percepção para insetos;", 10, 300);
+      text("- azul é a área de percepção para venenos;", 10, 320);
+      text("- amarelo é a área de percepção para predadores/presas.", 10, 340);
+      text("- branco é a área de percepção para carnes.", 10, 360);
+      text("Linhas que saem pela frente e por trás das criaturas são forças de atração e repulsão, respectivamente:", 10, 390);
+      text("- verde é a força de atração/repulsão por plantas;", 10, 410);
+      text("- vermelho é a força de atração/repulsão por insetos;", 10, 430);
+      text("- azul é a força de atração/repulsão por venenos;", 10, 450);
+      text("- amarelo é a força de atração/repulsão por predadores/presas.", 10, 470);
+      text("- branco é a força de atração/repulsão por carnes.", 10, 490);
+    }
   }
+}
+
+//______________________________________________________________________________
+// método que ativa o modo debug com o botão do mouse
+//______________________________________________________________________________
+function mousePressed(){
+  this.debug = !this.debug;
 }
 
 //______________________________________________________________________________
